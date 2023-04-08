537a054747c84cc6e3ef7833952a98236fca60ed
FayizTv <tvfayiztv@gmail.com>
2023-04-08 14:04:04 +0530

    config added

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -3,7 +3,7 @@ function dbConnect(){
     mongoose.connect("mongodb://127.0.0.1/olx-clone").then(result=>{
         console.log("Database connected")
     }).catch((err)=>{
-        console.log("data base error \n"+err)
+        console.log("Database error \n"+err)
     })
 }
 export default dbConnect
