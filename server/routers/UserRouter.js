aafbcb28d589f0e3d2ddae65feb6497d711cea95
FayizTv <tvfayiztv@gmail.com>
2023-04-08 14:21:25 +0530

    user router completed

--- server/routers/UserRouter.js
+++ server/routers/UserRouter.js
@@ -1,6 +1,35 @@
-import express from "express";
-import multer from "multer"
+import  express from "express";
+import multer from "multer";
+import { addProduct, getCategories, getProduct, getProducts } from "../controllers/ProductController.js";
+import { checkUserLoggedIn, userLogin, userLogout, userRegister } from "../controllers/UserController.js";
 
-const router = express.Router();
+
+
+
+const router=express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now()+".jpg"
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+const upload = multer({ storage: storage })
+
+
+router.get("/", (req, res)=>{res.json("hai")})
+router.post("/register", userRegister)
+router.post("/login", userLogin)
+router.post('/add-product', upload.single('image'), addProduct )
+router.get('/products', getProducts )
+router.get("/logout", userLogout)
+router.get('/product/:id', getProduct )
+router.get("/check-auth", checkUserLoggedIn)
+router.get("/categories", getCategories)
+
 
 export default router
